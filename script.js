6fc88804e3e49969e68bf74c878a35b1880e727f
Adarsh Singh <singh06vikash@gmail.com>
2024-12-03 23:13:54 +0530

    few more changes

--- script.js
+++ script.js
@@ -73,23 +73,28 @@ const getWeather = (city,chart_lable) => {
           const temp3 = Math.round(response.items[3].temperature.max);
           const temp4 = Math.round(response.items[4].temperature.max);
           const temp5 = Math.round(response.items[5].temperature.max);
+          const temp6 = Math.round(response.items[6].temperature.max);
+          const temp7 = Math.round(response.items[7].temperature.max);
 
           const min_temp1 = Math.round(response.items[1].temperature.min);
           const min_temp2 = Math.round(response.items[2].temperature.min);
           const min_temp3 = Math.round(response.items[3].temperature.min);
           const min_temp4 = Math.round(response.items[4].temperature.min);
           const min_temp5 = Math.round(response.items[5].temperature.min);
+          const min_temp6 = Math.round(response.items[6].temperature.min);
+          const min_temp7 = Math.round(response.items[7].temperature.min);
 
-
-          const temperatureData = [temp1,temp2,temp3,temp3,temp4,temp5];
-          const temperatureDataMin = [min_temp1,min_temp2,min_temp3,min_temp4,min_temp5];
+          const temperatureData = [temp1,temp2,temp3,temp3,temp4,temp5,temp6,temp7];
+          const temperatureDataMin = [min_temp1,min_temp2,min_temp3,min_temp4,min_temp5,min_temp6,min_temp7];
 
           const date1 = response.items[1].date;
           const date2 = response.items[2].date;
           const date3 = response.items[3].date;
           const date4 = response.items[4].date;
           const date5 = response.items[5].date;
-          let chart_lable = [date1,date2,date3,date4,date5];
+          const date6 = response.items[6].date;
+          const date7 = response.items[7].date;
+          let chart_lable = [date1,date2,date3,date4,date5,date6,date7];
         // update TemperatureChartMin(temperatureDataMin,chart_lable);  
         updateTemperatureChart(temperatureData,temperatureDataMin,chart_lable);
         // updateTemperatureChart(temperatureDataMin,chart_lable);
@@ -106,10 +111,10 @@ const chart_max = document.getElementById('temperatureChart').getContext('2d');
 const temperatureChart = new Chart(chart_max, {
   type: 'line',
   data: {
-    labels: [0, 0, 0, 0, 0],
+    labels: [0, 0, 0, 0, 0,0,0],
     datasets: [{
       label: 'Temperature (°C)',
-      data: [0, 0, 0, 0, 0], // Initial data will be replaced
+      data: [0, 0, 0, 0, 0,0,0], // Initial data will be replaced
       fill: true,
       color: "rgba(255, 255, 255, 0.8)",
       backgroundColor: "rgba(75, 192, 192, 0.2)", // Fill under the curve
@@ -121,7 +126,7 @@ const temperatureChart = new Chart(chart_max, {
     },
     {
       label: "Min Temperature (°C)",
-      data: [0, 0, 0, 0, 0], // Initial data for Min Temp
+      data: [0, 0, 0, 0, 0,0,0], // Initial data for Min Temp
       fill: true,
       borderColor: "rgba(54, 162, 235, 1)", // Line color for Min Temp
       pointBackgroundColor: "rgba(54, 162, 235, 1)", // Point color for Min Temp
